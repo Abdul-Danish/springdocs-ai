dd822821eef646155b16b7edc6964f3aea58be0f
Danish <danish@digitaldots.ai>
2026-07-05 12:39:44 +0530

    code cleanup & add thumbnail for project

--- src/main/java/com/danish/springrag/controller/ChatController.java
+++ src/main/java/com/danish/springrag/controller/ChatController.java
@@ -1,22 +1,5 @@
 package com.danish.springrag.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.evaluation.RelevancyEvaluator;
-import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.document.Document;
-import org.springframework.ai.evaluation.EvaluationRequest;
-import org.springframework.ai.vectorstore.SearchRequest;
-import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +14,6 @@ import reactor.core.publisher.Flux;
 @RestController
 @RequestMapping("/api/v1")
 @CrossOrigin(origins = "*")
-@Slf4j
 public class ChatController {
 
 	private final ChatService chatService;
